4747617187b55e3eb98c00d7a9313f9dbe8cd2fd
AmjadRabby <glamarrabby@gmail.com>
2020-09-26 23:23:43 +0600

    node simpale

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 const express = require('express');
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
 const app = express();
+app.use(cors())
+app.use(bodyParser.json())
+
+   //GET
+
+const users = ["amjad", "aziz", "essa", "rahul", "tapim"]
 
 app.get('/', (req, res) => {
-    res.send("Thank you for calling me amjad 2200");
+    const fruit = {
+        product: 'apple',
+        price: 10   
+    };
+    res.send(fruit);
+})
+app.get('/fruits/banana', (req, res) => {
+    res.send({fruit: 'banana', quantity: 1000, price:10000});
+})
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    const name = users[id];
+    res.send({id, name})
 })
 
-app.listen(2200, () => console.log("Listenting to port 2200"));
+    //POST 
+
+    app.post('/addUser', (req, res) => {
+        //save to database
+        const user = req.body;
+        user.id = 10;
+        res.send(user);
+    })
+
+app.listen(4000, () => console.log("Listenting to port 4000"));
